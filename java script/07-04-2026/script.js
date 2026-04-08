0ad31a2bb9db79ce1a35fef865222ff9339c2f97
ayush dudhat <ayushdudhat62@gmail.com>
2026-04-08 10:04:41 +0530

    07-04-2026

---
+++ java script/07-04-2026/script.js
@@ -0,0 +1,40 @@
+// localStorage
+
+const profile = {
+    name:"ayush",
+    email:"ayush@gmail.com"
+}
+
+const data = JSON.stringify(profile)
+
+const setData = () => {
+    localStorage.setItem("user" , data)
+    localStorage.setItem("users" , data)
+}
+
+const getData = () => {
+    let user = localStorage.getItem("user")
+    let profile = JSON.parse(user)
+    console.log(profile);
+    document.getElementById("name").innerText = profile.name
+    document.getElementById("email").innerText = profile.email
+}
+
+
+const removeData = () => {
+    localStorage.removeItem("user")
+}
+
+const clearData = () => {
+    localStorage.clear()
+}
+
+//session - Storage
+
+let SecretKey = "HusssbJHBAHS5454"
+
+let key = JSON.stringify(SecretKey)
+
+const sessionSetData = () => {
+    sessionStorage.setItem("auth" , key)
+}
