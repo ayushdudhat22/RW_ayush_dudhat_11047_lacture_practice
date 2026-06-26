6d7f72205b60852ed17199ffeeda360b8d7304b6
ayush dudhat <ayushdudhat62@gmail.com>
2026-06-26 09:47:10 +0530

    25-06-2026

--- react.js/17-06-2026/index.js
+++ react.js/17-06-2026/index.js
@@ -1,5 +1,5 @@
 // Javascript Rest Operator (...)
-
+ 
 // Function Parameter
 
 function sum(...numbers){
@@ -141,3 +141,28 @@ const groupedStudents = students.reduce((accumulator, currentStudent) => {
 }, {});
 
 console.log(groupedStudents);
+
+import './App.css'
+import { useState } from 'react'
+
+function App() {
+
+  const [count, setCount] = useState(0)
+  const [name , setName] = useState('')
+
+  console.log(count , name);
+  
+
+  return (
+    <>
+      <h1>count : {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+      <input type="text" onChange={(e) => setName(e.target.value)}/>
+      <div>Name : {name}</div>
+    </>
+  )
+}
+
+export default App
